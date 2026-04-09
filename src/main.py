42ebb16b0615a8c8ba4be2cb16cfd5baecbf0b0a
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-09 15:01:17 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
-from omop_calc_sofa import compute_omop_sofa
+from omop_utils import *
+from omop_calc_sofa import compute_daily_sofa
 from omop_calc_sepsis3 import compute_suspected_infection, evaluate_sepsis3
 
-# 1. Generate longitudinal organ failure scores
-longitudinal_sofa = compute_omop_sofa(cdm, cdm_ancestor)
-
-# 2. Identify clinical infection windows
-t_inf_cohort = compute_suspected_infection(cdm, cdm_ancestor)
-
-# 3. Apply Sepsis-3 phenotype constraints
-final_sepsis3_cohort = evaluate_sepsis3(longitudinal_sofa, t_inf_cohort)
+# load your CDM dict and concept_ancestor
+daily_sofa = compute_daily_sofa(cdm, cdm_ancestor)
+suspected = compute_suspected_infection(cdm, cdm_ancestor)
+sepsis3 = evaluate_sepsis3(daily_sofa, suspected, cdm, cdm_ancestor)
