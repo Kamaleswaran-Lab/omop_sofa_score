2d9475e06db6eb19942baedba5dbf48ec6d98266
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-12 11:03:54 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from omop_utils import *
-from omop_calc_sofa import compute_daily_sofa
-from omop_calc_sepsis3 import compute_suspected_infection, evaluate_sepsis3
+import psycopg2
+from src.omop_utils import set_schemas, set_verbose
+from src.omop_calc_sofa import compute_daily_sofa, compute_hourly_sofa
+from src.omop_calc_sepsis3 import compute_suspected_infection, evaluate_sepsis3
 
-# load your CDM dict and concept_ancestor
-daily_sofa = compute_daily_sofa(cdm, cdm_ancestor)
-suspected = compute_suspected_infection(cdm, cdm_ancestor)
-sepsis3 = evaluate_sepsis3(daily_sofa, suspected, cdm, cdm_ancestor)
+set_schemas(clinical="omopcdm", vocab="vocabulary")
+set_verbose(True)
+
+conn = psycopg2.connect(dbname="mgh", user="...", host="...", password="...")
+
+# hourly then daily (matches README API)
+hourly = compute_hourly_sofa(db_conn=conn, person_ids=[1907]) # or None for all
+daily = compute_daily_sofa(db_conn=conn, person_ids=[1907])
+
+# sepsis-3
+suspected = compute_suspected_infection(db_conn=conn, person_ids=[1907])
+sepsis3 = evaluate_sepsis3(hourly, suspected)
