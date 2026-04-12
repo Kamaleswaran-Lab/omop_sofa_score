2d69b60d165dce9fc2d6f01f44bbe77a599b08f9
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-12 13:48:34 -0400

    Delete sql directory

--- sql/create_indexes.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_meas_pt ON cdm.measurement(person_id, measurement_datetime);
-CREATE INDEX idx_drug_pt ON cdm.drug_exposure(person_id, drug_exposure_start_datetime);
-CREATE INDEX idx_device_pt ON cdm.device_exposure(person_id, device_exposure_start_datetime);
-CREATE INDEX idx_proc_pt ON cdm.procedure_occurrence(person_id, procedure_datetime);
