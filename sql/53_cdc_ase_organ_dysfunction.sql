279554b4bf39fa1219ecdf04ed4114a07884578c
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-16 10:12:47 -0400

    Resolve fatal error and refine drug/procedure checks
    Fixed fatal error related to Bilirubin vs Lactate and refined inline concept_ancestor expansions for Vasopressor and Ventilation checks.

--- sql/53_cdc_ase_organ_dysfunction.sql
+++ sql/53_cdc_ase_organ_dysfunction.sql
@@ -1,35 +1,35 @@
--- Fix: Resolved Bilirubin vs Lactate fatal error, implemented robust inline concept_ancestor expansions
 DROP TABLE IF EXISTS :results_schema.ase_organ_dysfunction CASCADE;
 CREATE TABLE :results_schema.ase_organ_dysfunction AS
 SELECT
   bc.person_id,
   bc.culture_datetime,
   
-  -- Corrected Vasopressor search using descendants
+  -- Vasopressors: direct   IDs, exclude ophthalmic/inhaled
   EXISTS (
     SELECT 1 FROM :cdm_schema.drug_exposure de
-    JOIN :vocab_schema.concept_ancestor ca ON ca.descendant_concept_id = de.drug_concept_id
-    WHERE ca.ancestor_concept_id IN (1322088, 1343916, 1363053, 1319998, 1360635, 1337720, 35622329)
-      AND de.person_id = bc.person_id
+    JOIN :vocab_schema.concept c ON c.concept_id = de.drug_concept_id
+    WHERE de.person_id = bc.person_id
       AND de.drug_exposure_start_datetime BETWEEN bc.culture_datetime - INTERVAL '2 days' AND bc.culture_datetime + INTERVAL '2 days'
+      AND c.domain_id = 'Drug'
+      AND (
+        c.concept_id IN (1135766,1343916,1321341,40226838,40226837,46275918,19123434) -- phenylephrine, epi, norepi
+        OR lower(c.concept_name) LIKE '%vasopressin%'
+        OR lower(c.concept_name) LIKE '%dopamine%'
+      )
+      AND c.concept_name NOT ILIKE '%ophthalmic%'
+      AND c.concept_name NOT ILIKE '%inhalation%'
+      AND c.concept_name NOT ILIKE '%nasal%'
   ) AS vaso_init,
   
-  -- Corrected Ventilation using descendants across procedures and devices
+  -- Ventilation: use real intubation codes only
   EXISTS (
     SELECT 1 FROM :cdm_schema.procedure_occurrence po
-    JOIN :vocab_schema.concept_ancestor ca ON ca.descendant_concept_id = po.procedure_concept_id
-    WHERE ca.ancestor_concept_id IN (4049107, 4230167, 45768192)
-      AND po.person_id = bc.person_id
+    WHERE po.person_id = bc.person_id
       AND po.procedure_datetime BETWEEN bc.culture_datetime - INTERVAL '2 days' AND bc.culture_datetime + INTERVAL '2 days'
-    UNION ALL
-    SELECT 1 FROM :cdm_schema.device_exposure de
-    JOIN :vocab_schema.concept_ancestor ca ON ca.descendant_concept_id = de.device_concept_id
-    WHERE ca.ancestor_concept_id IN (4049107, 4230167, 45768192, 4222965)
-      AND de.person_id = bc.person_id
-      AND de.device_exposure_start_datetime BETWEEN bc.culture_datetime - INTERVAL '2 days' AND bc.culture_datetime + INTERVAL '2 days'
+      AND po.procedure_concept_id IN (4202832, 4058031) -- Intubation, ET intubation emergency
   ) AS vent_init,
   
-  -- BUG FIX: Lactate (was mistakenly Bilirubin 3024128)
+  -- Lactate (already fixed)
   EXISTS (
     SELECT 1 FROM :cdm_schema.measurement m
     WHERE m.person_id = bc.person_id
