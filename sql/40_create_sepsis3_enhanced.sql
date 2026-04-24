e4b5fe9f96f76631ae9a0e8baec177a9f2fdc0ae
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-24 13:39:01 -0400

    Refactor sepsis3_enhanced table creation for results_site_a

--- sql/40_create_sepsis3_enhanced.sql
+++ sql/40_create_sepsis3_enhanced.sql
@@ -1,55 +1,59 @@
--- 40_create_sepsis3_enhanced.sql
-DROP TABLE IF EXISTS {{results_schema}}.sepsis3_enhanced CASCADE;
+DROP TABLE IF EXISTS results_site_a.sepsis3_enhanced CASCADE;
 
-CREATE TABLE {{results_schema}}.sepsis3_enhanced AS
-WITH infection_events AS (
-    SELECT DISTINCT ON (person_id, visit_occurrence_id)
+CREATE TABLE results_site_a.sepsis3_enhanced AS
+WITH onset AS (
+    SELECT 
+        person_id,
+        visit_occurrence_id,
+        infection_onset,
+        infection_type
+    FROM results_site_a.infection_onset_enhanced
+),
+sofa_window AS (
+    SELECT 
+        o.person_id,
+        o.visit_occurrence_id,
+        o.infection_onset,
+        o.infection_type,
+        s.charttime,
+        s.sofa_total,
+        EXTRACT(EPOCH FROM (s.charttime - o.infection_onset))/3600 AS hours_from_onset
+    FROM onset o
+    JOIN results_site_a.sofa_hourly s
+      ON s.person_id = o.person_id
+     AND s.charttime BETWEEN o.infection_onset - INTERVAL '48 hours'
+                         AND o.infection_onset + INTERVAL '72 hours'
+),
+baseline_calc AS (
+    SELECT
         person_id,
         visit_occurrence_id,
         infection_onset,
         infection_type,
-        antibiotic_start,
-        culture_start
-    FROM {{results_schema}}.infection_onset_enhanced
-    ORDER BY person_id, visit_occurrence_id, infection_onset
-),
-sofa_with_baseline AS (
-    SELECT 
-        i.person_id,
-        i.visit_occurrence_id,
-        i.infection_onset,
-        i.infection_type,
-        COALESCE(
-            (SELECT MAX(sh.total_sofa) 
-             FROM {{results_schema}}.sofa_hourly sh
-             WHERE sh.person_id = i.person_id
-               AND sh.charttime BETWEEN i.infection_onset - interval '48 hours'
-                                    AND i.infection_onset - interval '1 hour'),
-            0
-        ) AS baseline_sofa,
-        (SELECT MAX(sh.total_sofa)
-         FROM {{results_schema}}.sofa_hourly sh
-         WHERE sh.person_id = i.person_id
-           AND sh.charttime BETWEEN i.infection_onset - interval '6 hours'
-                                AND i.infection_onset + interval '24 hours')
-        AS peak_sofa
-    FROM infection_events i
+        -- Best baseline: minimum SOFA in 24h before onset
+        MIN(sofa_total) FILTER (WHERE hours_from_onset BETWEEN -24 AND -1) AS baseline_24h_min,
+        -- Fallback: last SOFA in 24-48h before
+        (SELECT sofa_total FROM sofa_window sw2 
+         WHERE sw2.person_id = sw.person_id 
+           AND sw2.visit_occurrence_id = sw.visit_occurrence_id
+           AND sw2.hours_from_onset BETWEEN -48 AND -24
+         ORDER BY sw2.charttime DESC LIMIT 1) AS baseline_48h_last,
+        MAX(sofa_total) FILTER (WHERE hours_from_onset BETWEEN 0 AND 72) AS peak_72h,
+        MIN(charttime) FILTER (WHERE hours_from_onset BETWEEN 0 AND 72) AS first_sofa_time
+    FROM sofa_window sw
+    GROUP BY person_id, visit_occurrence_id, infection_onset, infection_type
 )
-SELECT DISTINCT ON (person_id, visit_occurrence_id)
+SELECT
     person_id,
     visit_occurrence_id,
     infection_onset,
     infection_type,
-    baseline_sofa,
-    peak_sofa,
-    (peak_sofa - baseline_sofa) AS delta_sofa,
-    CASE 
-        WHEN (peak_sofa - baseline_sofa) >= 2 THEN TRUE 
-        ELSE FALSE 
-    END AS meets_sepsis3
-FROM sofa_with_baseline
-WHERE peak_sofa IS NOT NULL
-ORDER BY person_id, visit_occurrence_id, infection_onset;
+    COALESCE(baseline_24h_min, baseline_48h_last, 0) AS baseline_sofa,
+    (baseline_24h_min IS NULL AND baseline_48h_last IS NULL) AS baseline_imputed,
+    peak_72h AS peak_sofa,
+    peak_72h - COALESCE(baseline_24h_min, baseline_48h_last, 0) AS delta_sofa,
+    (peak_72h - COALESCE(baseline_24h_min, baseline_48h_last, 0)) >= 2 AS meets_sepsis3
+FROM baseline_calc
+WHERE peak_72h IS NOT NULL;
 
-CREATE INDEX idx_sepsis3_enh_person ON {{results_schema}}.sepsis3_enhanced(person_id);
-CREATE INDEX idx_sepsis3_enh_visit ON {{results_schema}}.sepsis3_enhanced(visit_occurrence_id);
+CREATE INDEX ON results_site_a.sepsis3_enhanced(person_id, infection_onset);
