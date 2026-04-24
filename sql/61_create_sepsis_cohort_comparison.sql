8d48277d9173d29cbdb60d3754d6d089f008a531
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-24 12:58:13 -0400

    Modify sepsis cohort comparison SQL script
    Updated SQL script to create a comparison table for Sepsis-3 and CDC ASE cohorts. Added infection_type and created an index on sepsis3_onset for performance.

--- sql/61_create_sepsis_cohort_comparison.sql
+++ sql/61_create_sepsis_cohort_comparison.sql
@@ -1,7 +1,6 @@
--- 61_create_sepsis_cohort_comparison_mgh.sql
--- matches actual column names in results_site_a
--- Fixes: infection_onset (not infection_onset_datetime / ase_onset_datetime)
--- Requires: results_site_a.sepsis3_enhanced, results_site_a.cdc_ase_cohort_final
+-- 61_create_sepsis_cohort_comparison.sql
+-- Compares Sepsis-3 vs CDC ASE cohorts
+-- MGH-fixed: uses infection_onset (not infection_onset_datetime)
 
 DROP TABLE IF EXISTS @results_schema.sepsis_cohort_comparison CASCADE;
 
@@ -11,6 +10,7 @@ WITH sepsis3 AS (
         person_id,
         visit_occurrence_id,
         infection_onset AS sepsis3_onset,
+        infection_type,
         baseline_sofa,
         peak_sofa,
         delta_sofa
@@ -24,12 +24,14 @@ ase AS (
         person_id,
         visit_occurrence_id,
         infection_onset AS ase_onset,
+        onset_date,
         max_sofa_72h,
         sofa_severity,
         vasopressor_72h,
         ventilation_72h,
         onset_type,
         died_in_hospital,
+        died_30d,
         hospital_los_days
     FROM @results_schema.cdc_ase_cohort_final
 )
@@ -38,6 +40,7 @@ SELECT
     COALESCE(s.visit_occurrence_id, a.visit_occurrence_id) AS visit_occurrence_id,
     s.sepsis3_onset,
     a.ase_onset,
+    s.infection_type,
     s.baseline_sofa,
     s.peak_sofa,
     s.delta_sofa,
@@ -47,6 +50,7 @@ SELECT
     a.ventilation_72h,
     a.onset_type,
     a.died_in_hospital,
+    a.died_30d,
     a.hospital_los_days,
     CASE
         WHEN s.person_id IS NOT NULL AND a.person_id IS NOT NULL THEN 'both'
@@ -59,12 +63,6 @@ FULL OUTER JOIN ase a
   ON s.person_id = a.person_id
  AND s.visit_occurrence_id = a.visit_occurrence_id;
 
--- Indexes for performance
 CREATE INDEX idx_scc_person ON @results_schema.sepsis_cohort_comparison(person_id);
 CREATE INDEX idx_scc_cohort ON @results_schema.sepsis_cohort_comparison(cohort_type);
-
--- Validation query
--- SELECT cohort_type, COUNT(*) as n,
---        ROUND(100.0*COUNT(*)/SUM(COUNT(*)) OVER (),1) as pct
--- FROM @results_schema.sepsis_cohort_comparison
--- GROUP BY cohort_type;
+CREATE INDEX idx_scc_onset ON @results_schema.sepsis_cohort_comparison(sepsis3_onset);
