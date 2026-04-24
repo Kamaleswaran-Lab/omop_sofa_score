d7dc289580bb15af99012128179abbbf736028c0
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-23 21:37:26 -0400

    Refactor sepsis3_enhanced SQL for schema flexibility

--- sql/40_create_sepsis3_enhanced.sql
+++ sql/40_create_sepsis3_enhanced.sql
@@ -1,9 +1,9 @@
 -- 40_create_sepsis3_enhanced.sql
--- FIXED: Real baseline_sofa from 48h pre-infection, deduplicated per visit
+-- FIXED: Real baseline from 48h pre-infection, deduplicated
 
-DROP TABLE IF EXISTS results_site_a.sepsis3_enhanced CASCADE;
+DROP TABLE IF EXISTS {{results_schema}}.sepsis3_enhanced CASCADE;
 
-CREATE TABLE results_site_a.sepsis3_enhanced AS
+CREATE TABLE {{results_schema}}.sepsis3_enhanced AS
 WITH infection_events AS (
     SELECT DISTINCT ON (person_id, visit_occurrence_id)
         person_id,
@@ -12,7 +12,7 @@ WITH infection_events AS (
         infection_type,
         antibiotic_start,
         culture_start
-    FROM results_site_a.infection_onset_enhanced
+    FROM {{results_schema}}.infection_onset_enhanced
     ORDER BY person_id, visit_occurrence_id, infection_onset
 ),
 sofa_with_baseline AS (
@@ -21,36 +21,33 @@ sofa_with_baseline AS (
         i.visit_occurrence_id,
         i.infection_onset,
         i.infection_type,
-        -- REAL baseline: mean SOFA 6-48h before infection
         COALESCE(
             (SELECT AVG(sh.total_sofa) 
-             FROM results_site_a.sofa_hourly sh
+             FROM {{results_schema}}.sofa_hourly sh
              WHERE sh.person_id = i.person_id
                AND sh.visit_occurrence_id = i.visit_occurrence_id
                AND sh.charttime BETWEEN i.infection_onset - interval '48 hours'
                                     AND i.infection_onset - interval '6 hours'),
             0
         ) AS baseline_sofa,
-        -- Peak SOFA in 24h after infection
         (SELECT MAX(sh.total_sofa)
-         FROM results_site_a.sofa_hourly sh
+         FROM {{results_schema}}.sofa_hourly sh
          WHERE sh.person_id = i.person_id
            AND sh.visit_occurrence_id = i.visit_occurrence_id
            AND sh.charttime BETWEEN i.infection_onset - interval '6 hours'
                                 AND i.infection_onset + interval '24 hours')
         AS peak_sofa,
-        -- Worst organ scores
-        (SELECT MAX(sh.resp_sofa) FROM results_site_a.sofa_hourly sh
+        (SELECT MAX(sh.resp_sofa) FROM {{results_schema}}.sofa_hourly sh
          WHERE sh.person_id = i.person_id AND sh.charttime BETWEEN i.infection_onset - interval '6h' AND i.infection_onset + interval '24h') AS resp_max,
-        (SELECT MAX(sh.coag_sofa) FROM results_site_a.sofa_hourly sh
+        (SELECT MAX(sh.coag_sofa) FROM {{results_schema}}.sofa_hourly sh
          WHERE sh.person_id = i.person_id AND sh.charttime BETWEEN i.infection_onset - interval '6h' AND i.infection_onset + interval '24h') AS coag_max,
-        (SELECT MAX(sh.liver_sofa) FROM results_site_a.sofa_hourly sh
+        (SELECT MAX(sh.liver_sofa) FROM {{results_schema}}.sofa_hourly sh
          WHERE sh.person_id = i.person_id AND sh.charttime BETWEEN i.infection_onset - interval '6h' AND i.infection_onset + interval '24h') AS liver_max,
-        (SELECT MAX(sh.cardio_sofa) FROM results_site_a.sofa_hourly sh
+        (SELECT MAX(sh.cardio_sofa) FROM {{results_schema}}.sofa_hourly sh
          WHERE sh.person_id = i.person_id AND sh.charttime BETWEEN i.infection_onset - interval '6h' AND i.infection_onset + interval '24h') AS cardio_max,
-        (SELECT MAX(sh.cns_sofa) FROM results_site_a.sofa_hourly sh
+        (SELECT MAX(sh.cns_sofa) FROM {{results_schema}}.sofa_hourly sh
          WHERE sh.person_id = i.person_id AND sh.charttime BETWEEN i.infection_onset - interval '6h' AND i.infection_onset + interval '24h') AS cns_max,
-        (SELECT MAX(sh.renal_sofa) FROM results_site_a.sofa_hourly sh
+        (SELECT MAX(sh.renal_sofa) FROM {{results_schema}}.sofa_hourly sh
          WHERE sh.person_id = i.person_id AND sh.charttime BETWEEN i.infection_onset - interval '6h' AND i.infection_onset + interval '24h') AS renal_max
     FROM infection_events i
 )
@@ -76,9 +73,9 @@ FROM sofa_with_baseline
 WHERE peak_sofa IS NOT NULL
 ORDER BY person_id, visit_occurrence_id, infection_onset;
 
--- Indexes
-CREATE INDEX idx_sepsis3_enhanced_person ON results_site_a.sepsis3_enhanced(person_id);
-CREATE INDEX idx_sepsis3_enhanced_visit ON results_site_a.sepsis3_enhanced(visit_occurrence_id);
-CREATE INDEX idx_sepsis3_enhanced_meets ON results_site_a.sepsis3_enhanced(meets_sepsis3) WHERE meets_sepsis3 = TRUE;
+CREATE INDEX idx_sepsis3_enh_person ON {{results_schema}}.sepsis3_enhanced(person_id);
+CREATE INDEX idx_sepsis3_enh_visit ON {{results_schema}}.sepsis3_enhanced(visit_occurrence_id);
+CREATE INDEX idx_sepsis3_enh_onset ON {{results_schema}}.sepsis3_enhanced(infection_onset);
+CREATE INDEX idx_sepsis3_enh_meets ON {{results_schema}}.sepsis3_enhanced(meets_sepsis3) WHERE meets_sepsis3 = TRUE;
 
-ANALYZE results_site_a.sepsis3_enhanced;
+ANALYZE {{results_schema}}.sepsis3_enhanced;
