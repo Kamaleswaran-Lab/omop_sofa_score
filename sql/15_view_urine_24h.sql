3da37e3921bcba3f92ac73f18e287e57b3c6b879
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-25 12:37:16 -0400

    Modify vw_urine_24h view to include visit details
    Updated the urine output view to include visit occurrence ID and adjusted the query to join with visit occurrence data.

--- sql/15_view_urine_24h.sql
+++ sql/15_view_urine_24h.sql
@@ -1,16 +1,16 @@
--- 24-hour rolling urine output
-DROP VIEW IF EXISTS results_site_a.vw_urine_24h CASCADE;
-
-CREATE OR REPLACE VIEW results_site_a.vw_urine_24h AS
-SELECT 
-    person_id,
-    measurement_datetime AS charttime,
-    value_as_number AS urine_hourly,
-    SUM(value_as_number) OVER (
-        PARTITION BY person_id 
-        ORDER BY measurement_datetime 
-        RANGE BETWEEN INTERVAL '24 hours' PRECEDING AND CURRENT ROW
-    ) AS urine_24h
-FROM omopcdm.measurement
-WHERE measurement_concept_id = 4264378  -- Urine output - 2,203,519 records
-AND value_as_number IS NOT NULL;
+CREATE OR REPLACE VIEW :results_schema.vw_urine_24h AS
+SELECT person_id, visit_occurrence_id, measurement_datetime AS charttime,
+  SUM(value_as_number) OVER (
+    PARTITION BY person_id, visit_occurrence_id
+    ORDER BY measurement_datetime
+    RANGE BETWEEN INTERVAL '24 hours' PRECEDING AND CURRENT ROW
+  ) AS urine_24h
+FROM (
+  SELECT m.person_id, m.value_as_number, m.measurement_datetime,
+         vo.visit_occurrence_id
+  FROM :cdm_schema.measurement m
+  JOIN :cdm_schema.visit_occurrence vo
+    ON m.person_id=vo.person_id
+   AND m.measurement_datetime BETWEEN vo.visit_start_datetime AND vo.visit_end_datetime
+  WHERE m.measurement_concept_id=4264378 AND m.value_as_number IS NOT NULL
+) x;
