5a31aafc17969bebc55efc2088cdfda6ba1cccd3
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-26 18:00:13 -0400

    Fix pao2_time to pao2_datetime in view definition

--- sql/30_view_sofa_components.sql
+++ sql/30_view_sofa_components.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE VIEW :results_schema.vw_sofa_components AS
 SELECT b.person_id, b.hr,
   pf.pf_ratio, l.platelets, l.bilirubin, v.map, n.gcs_total, l.creatinine, u.urine_24h_ml, r.rrt_active
 FROM (SELECT person_id, generate_series(MIN(measurement_datetime), MAX(measurement_datetime), '1 hour') AS hr FROM :cdm_schema.measurement GROUP BY 1) b
-LEFT JOIN :results_schema.view_pao2_fio2_pairs pf ON pf.person_id=b.person_id AND pf.pao2_time BETWEEN b.hr - interval '2h' AND b.hr
+LEFT JOIN :results_schema.view_pao2_fio2_pairs pf ON pf.person_id=b.person_id AND pf.pao2_datetime BETWEEN b.hr - interval '2h' AND b.hr
 LEFT JOIN :results_schema.view_labs_core l ON l.person_id=b.person_id AND l.measurement_datetime BETWEEN b.hr - interval '24h' AND b.hr
 LEFT JOIN :results_schema.view_vitals_core v ON v.person_id=b.person_id AND v.measurement_datetime BETWEEN b.hr - interval '1h' AND b.hr
 LEFT JOIN :results_schema.view_neuro n ON n.person_id=b.person_id AND n.measurement_datetime BETWEEN b.hr - interval '24h' AND b.hr
