fb95c35627278634a747348d30a6afc677f1a704
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-26 17:51:50 -0400

    Update SQL script to use parameterized schema

--- sql/51_cdc_ase_blood_cultures.sql
+++ sql/51_cdc_ase_blood_cultures.sql
@@ -1,8 +1,8 @@
 -- 51_cdc_ase_blood_cultures.sql
 -- Site A edit: use infection_onset_enhanced instead of strict blood culture
 
-DROP TABLE IF EXISTS {{results_schema}}.cdc_ase_cultures CASCADE;
-CREATE TABLE {{results_schema}}.cdc_ase_cultures AS
+DROP TABLE IF EXISTS :results_schema.cdc_ase_cultures CASCADE;
+CREATE TABLE :results_schema.cdc_ase_cultures AS
 SELECT
   person_id,
   visit_occurrence_id,
@@ -10,7 +10,7 @@ SELECT
   culture_start,
   antibiotic_start,
   culture_site
-FROM {{results_schema}}.view_infection_onset_enhanced
+FROM :results_schema.view_infection_onset_enhanced
 WHERE culture_start IS NOT NULL
   AND antibiotic_start IS NOT NULL
   AND ABS(EXTRACT(EPOCH FROM (antibiotic_start - culture_start))/3600) <= 96;
