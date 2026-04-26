602eee5f4ae31d45f78a77ea4482436f27c28aa1
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-26 17:42:55 -0400

    Delete sql/RUN_ALL_enhanced_v4.5.sql

--- sql/RUN_ALL_enhanced_v4.5.sql
+++
@@ -1,4 +0,0 @@
--- FIXED: removed 40484543,40486635,2072499989; removed WHERE icu_onset=1
-\set ON_ERROR_STOP on
-SELECT * FROM :cdm_schema.visit_detail WHERE visit_detail_concept_id IN (32037,581379); -- portable ICU, no B2AI
--- No icu_onset filter for full inpatient trajectories
