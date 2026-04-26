9b5893117da044d3022ceb33b31644a8852234a1
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-26 18:02:12 -0400

    Enhance RUN_ALL_enhanced.sql with schema management
    Updated SQL script to include necessary DROP VIEW and DROP TABLE statements for better management of schema changes.

--- sql/RUN_ALL_enhanced.sql
+++ sql/RUN_ALL_enhanced.sql
@@ -1,33 +1,48 @@
--- RUN_ALL_enhanced_FIXED.sql
-\echo '=== 0. Schemas ==='
-\ir 00_create_schemas.sql
+-- RUN_ALL_MGH_FIXED.sql - works with Kamaleswaran-Lab repo as-is
+\set results_schema results_site_a
+\set cdm_schema omopcdm
+\set vocab_schema vocabulary
 
-\echo '=== 1. Core views ==='
+\ir 00_create_schemas.sql
 \ir 10_view_labs_core.sql
 \ir 11_view_vitals_core.sql
+\ir 12_view_vasopressors_nee.sql
+\ir 13_view_ventilation.sql
 \ir 14_view_neuro.sql
 \ir 15_view_urine_24h.sql
 \ir 16_view_rrt.sql
 
-\echo '=== 2. Respiratory & support ==='
+-- Fix PF view first
+DROP VIEW IF EXISTS :results_schema.view_pao2_fio2_pairs CASCADE;
 \ir 20_view_pao2_fio2_pairs.sql
-\ir 12_view_vasopressors_nee.sql
-\ir 13_view_ventilation.sql
 
-\echo '=== 3. Microbiology ==='
-\ir 22_view_cultures.sql
+-- Recreate with CASCADE to avoid "cannot drop columns"
+DROP VIEW IF EXISTS :results_schema.view_antibiotics CASCADE;
 \ir 21_view_antibiotics.sql
-\ir 23_view_infection_onset_enhanced.sql
+DROP VIEW IF EXISTS :results_schema.view_cultures CASCADE;
+\ir 22_view_cultures.sql
+DROP VIEW IF EXISTS :results_schema.view_infection_onset CASCADE;
+\ir 23_view_infection_onset.sql
 
-\echo '=== 4. SOFA components ==='
+-- Patch SOFA components for pao2_datetime
+DROP VIEW IF EXISTS :results_schema.vw_sofa_components CASCADE;
 \ir 30_view_sofa_components.sql
+-- If error about pao2_time, run this manual fix once:
+-- sed -i 's/pao2_time/pao2_datetime/g' 30_view_sofa_components.sql
+
+DROP TABLE IF EXISTS :results_schema.sofa_hourly CASCADE;
 \ir 31_create_sofa_hourly.sql
 
-\echo '=== 5. Sepsis-3 ==='
+-- Sepsis-3
+DROP TABLE IF EXISTS :results_schema.sepsis3 CASCADE;
+\ir 40_create_sepsis3.sql
+DROP TABLE IF EXISTS :results_schema.sepsis3_enhanced CASCADE;
 \ir 40_create_sepsis3_enhanced.sql
+DROP TABLE IF EXISTS :results_schema.sepsis3_enhanced_collapsed CASCADE;
 \ir 41_create_sepsis3_collapsed_48h.sql
 
-\echo '=== 6. CDC ASE ==='
+-- ASE - must replace Jinja first
+\! sed -i 's/{{results_schema}}/:results_schema/g; s/{{cdm_schema}}/:cdm_schema/g; s/{{vocab_schema}}/:vocab_schema/g' 5*.sql
 \ir 50_cdc_ase_parameters.sql
 \ir 51_cdc_ase_blood_cultures.sql
 \ir 52_cdc_ase_qad.sql
@@ -36,10 +51,9 @@
 \ir 55_cdc_ase_with_sofa.sql
 \ir 56_cdc_ase_cohort_final.sql
 
-\echo '=== 7. Comparison ==='
+-- Comparison
+DROP TABLE IF EXISTS :results_schema.sepsis_cohort_comparison CASCADE;
 \ir 61_create_sepsis_cohort_comparison.sql
-\ir 60_sepsis_combined_sep3_ASE_characteristics.sql
 
-\echo '=== 8. Analyze ==='
 ANALYZE :results_schema.cdc_ase_cohort_final;
 ANALYZE :results_schema.sepsis_cohort_comparison;
