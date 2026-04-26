1faa66487cd70a71a889853b1a3ca8018d10df64
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-04-26 17:51:12 -0400

    Update SQL syntax for dropping and creating table

--- sql/50_cdc_ase_parameters.sql
+++ sql/50_cdc_ase_parameters.sql
@@ -1,8 +1,8 @@
 -- 50_cdc_ase_parameters.sql
 -- Site A parameters to match enhanced pipeline
 
-DROP TABLE IF EXISTS {{results_schema}}.cdc_ase_parameters;
-CREATE TABLE {{results_schema}}.cdc_ase_parameters AS
+DROP TABLE IF EXISTS :results_schema.cdc_ase_parameters;
+CREATE TABLE :results_schema.cdc_ase_parameters AS
 SELECT
   96 AS culture_window_hours,  -- match enhanced (was 48)
   4 AS qad_min_days,
